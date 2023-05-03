4fa85c6cfe03ab92a12839273aecd3d7f9331a73
ray <ray@DESKTOP-D6ATR02>
2023-05-03 01:08:33 -0500

    Add project files.

---
+++ Default ScriptHook Template/Default ScriptHook Template.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms; // Add this line to import the Keys namespace
+using GTA;
+using GTA.Math;
+
+// Replace "YourNamespace" with the namespace you want to use for your project
+namespace Default_ScriptHook_Template
+{
+    // Replace "YourScript" with a descriptive name for your script class
+    public class Default_ScriptHook_Template : Script
+    {
+        // Constructor
+        public Default_ScriptHook_Template()
+        {
+            // Subscribe to Tick event (called on every frame)
+            Tick += OnTick;
+
+            // Subscribe to Aborted event (called when the script is being stopped or unloaded)
+            Aborted += OnAbort;
+
+            // Subscribe to KeyDown event (called when a key is initially pressed)
+            KeyDown += OnKeyDown;
+
+            // Subscribe to KeyUp event (called when a key is released)
+            KeyUp += OnKeyUp;
+
+            // Add any additional setup code here
+        }
+
+        // The Tick event handler
+        private void OnTick(object sender, EventArgs e)
+        {
+            // Add your code here to be executed on every frame
+
+            // Example: Check if the player is in a police vehicle
+            // if (Game.Player.Character.IsInPoliceVehicle)
+            // {
+            //     // Your code here
+            // }
+        }
+
+        // The Abort event handler
+        private void OnAbort(object sender, EventArgs e)
+        {
+            // Add any cleanup code here, such as unsubscribing from events, stopping timers, or disposing resources
+
+            // Example: Unsubscribe from Tick, Aborted, KeyDown, and KeyUp events
+            // Tick -= OnTick;
+            // Aborted -= OnAbort;
+            // KeyDown -= OnKeyDown;
+            // KeyUp -= OnKeyUp;
+        }
+
+        // The KeyDown event handler
+        private void OnKeyDown(object sender, KeyEventArgs e)
+        {
+            // Add your code here to be executed when a key is initially pressed
+
+            // Example: Check if the J key is pressed
+            // if (e.KeyCode == Keys.J)
+            // {
+            //     // Your code here
+            // }
+        }
+
+        // The KeyUp event handler
+        private void OnKeyUp(object sender, KeyEventArgs e)
+        {
+            // Add your code here to be executed when a key is released
+
+            // Example: Check if the K key is released
+            // if (e.KeyCode == Keys.K)
+            // {
+            //     // Your code here
+            // }
+        }
+    }
+}
